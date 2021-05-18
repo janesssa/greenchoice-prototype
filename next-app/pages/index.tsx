f4e2507c108f48e03c1d1ca6ef0b75fe8f639864
Janessa <0935165@hr.nl>
2021-05-18 14:18:47 +0200

    Added light/dark mode

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,17 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import * as React from 'react'
-import styles from '../styles/Home.module.scss'
+import { useState } from 'react'
+import styles from 'styles/Home.module.scss'
+import useThemeDetector from 'utilities/hooks/useTheme'
+import Title from 'utilities/components/atoms/Title'
 
 type Props = {
   json: (e: React.FormEvent) => void
 }
 
 const Home:React.FC<Props>  = ({json}) => {
+  const [theme, setTheme] = useState(useThemeDetector())
   return (
-    <div className={styles.container}>
+    <div className={styles.container + theme}>
       <i></i>
       <b></b>
+      <Title></Title>
     </div>
   )
 }
