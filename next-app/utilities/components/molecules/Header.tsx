0e379a45fca49e462969fa9766c58fb76f0c9454
Janessa <0935165@hr.nl>
2021-05-19 12:01:43 +0200

    Added header style

---
+++ next-app/utilities/components/molecules/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from 'styles/molecules/Header.module.scss'
+
+const Header = () => {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
+
+export default Header
