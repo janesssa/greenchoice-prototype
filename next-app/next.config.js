812da0f794b306591556b3da8c16bf623caa00b0
Janessa <0935165@hr.nl>
2021-05-19 11:17:17 +0200

    cleanup

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -1,13 +1,16 @@
-// module.exports = {
-//     webpack: (config) => {
-//         return config;
-//     },
-//     webpackDevMiddleware: config => {
-//         config.watchOptions = {
-//             poll: 1000, // Check for changes every second
-//             aggregateTimeout: 300 // delay before rebuilding
-//         };
-//         return config;
-//     },
-//     distDir: ".next",
-// };
+module.exports = {
+    webpack: (config) => {
+        return config;
+    },
+    webpackDevMiddleware: config => {
+        config.watchOptions = {
+            poll: 1000, // Check for changes every second
+            aggregateTimeout: 300 // delay before rebuilding
+        };
+        return config;
+    },
+    distDir: ".next",
+    sassOptions: {
+        additionalData: '@use "styles/colors";'
+    }
+};
--- next-app/utilities/scss-resources.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-const resources = [
-  'styles/theme.scss'
-];
-module.exports = resources.map(file => path.resolve(__dirname, file));
