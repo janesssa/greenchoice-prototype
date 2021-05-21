9c120e7ea416084fc58038e052c0425e4ce799a7
Janessa <0935165@hr.nl>
2021-05-21 14:53:52 +0200

    Fixed gssp function

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,11 +1,32 @@
+import { useEffect } from 'react'
 import styles from 'styles/Home.module.scss'
 import Button from 'utilities/components/atoms/Button'
 import useUserData from 'utilities/hooks/useUserData'
 import { useHouseholdContext } from 'utilities/contexts/household-context'
 import Phone from 'utilities/components/atoms/Phone'
 
-const Home: React.FC = () => {
-  const { householdID, setHouseholdID } = useHouseholdContext()
+type HomeType = {
+  json: {
+    access_token: string
+  }
+}
+
+const Home: React.FC<HomeType> = ({ json }) => {
+  const { householdID, setHouseholdID, setAccessToken } = useHouseholdContext()
+
+  useEffect(() => {
+    setAccessToken(json.access_token)
+    console.log('set ' + json.access_token)
+    return () => {
+      setAccessToken('')
+    }
+  }, [json])
+
+
+  const onSubmit: () => void = () => {
+    const data = useUserData(householdID)
+    console.log(data)
+  }
 
   return (
     <Phone>
@@ -13,10 +34,10 @@ const Home: React.FC = () => {
         <div className={styles.formcontainer}>
           <h1>Welkom!</h1>
           <p>Voer hier je klantnummer in en krijg inzicht in je verbruik</p>
-          <form className={styles.form} onSubmit={() => useUserData(householdID)} >
+          <form className={styles.form}  >
             <label htmlFor="householdID">Klantnummer</label>
             <input type="text" name="householdID" value={householdID} onChange={(e) => setHouseholdID(e.target.value)} required />
-            <Button href="/live" text="Start!" />
+            <Button href="/live" text="Start!" handleClick={() => onSubmit()} />
           </form>
         </div>
       </div>
@@ -24,15 +45,23 @@ const Home: React.FC = () => {
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch('http://localhost:8080/api')
-  const json = await res.json()
 
-  return {
-    props: {
-      json
-    }
+export async function getServerSideProps() {
+  const options = {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      grant_type: "client_credentials",
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      audience: "https://api.onzo.com/engagement/"
+    })
   }
+
+  const res = await fetch("https://api.onzo.io/oauth/token", options);
+  const json = await res.json();
+
+  return { props: { json } };
 }
 
 export default Home
--- next-app/utilities/contexts/household-context.tsx
+++ next-app/utilities/contexts/household-context.tsx
@@ -3,11 +3,15 @@ import { createContext, useContext, useState, Dispatch, SetStateAction } from 'r
 type HouseholdType = {
     householdID: string,
     setHouseholdID: Dispatch<SetStateAction<string>>
+    access_token: string
+    setAccessToken: Dispatch<SetStateAction<string>>
 }
 
 export const HouseholdContext = createContext<HouseholdType>({
     householdID: '',
-    setHouseholdID: null
+    setHouseholdID: null,
+    access_token: '',
+    setAccessToken: null
 })
 
 export const useHouseholdContext = () => useContext(HouseholdContext)
@@ -16,9 +20,10 @@ export const useHouseholdContext = () => useContext(HouseholdContext)
 export const HouseholdProvider = ({ children }) => {
 
     const [householdID, setHouseholdID] = useState<string>('');
+    const [access_token, setAccessToken] = useState<string>('');
 
     return (
-        <HouseholdContext.Provider value={{ householdID, setHouseholdID }}>        
+        <HouseholdContext.Provider value={{ householdID, setHouseholdID, access_token, setAccessToken  }}>        
                         {children}    
         </HouseholdContext.Provider>
     )
