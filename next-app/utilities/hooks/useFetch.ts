6eeff38c36ae1c2cb22cc14ed1a0fd6467abc021
Janessa <0935165@hr.nl>
2021-05-23 13:31:59 +0200

    Insert props type

--- next-app/utilities/hooks/useFetch.ts
+++ next-app/utilities/hooks/useFetch.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function useFetch(url: RequestInfo, options ) {
+export default function useFetch(url: RequestInfo, options: RequestInit ) {
   const [response, setResponse] = useState([]);
   const [error, setError] = useState(null);
 
