36060fcb366fb6c95bd045b59e47228a8db4f0fa
Janessa <0935165@hr.nl>
2021-05-25 10:32:57 +0200

    Added pie chart and start of comparision

--- next-app/pages/devices/index.tsx
+++ next-app/pages/devices/index.tsx
@@ -13,11 +13,19 @@ const Devices = () => {
     const [values, setValues] = useState([])
     const [selection, setSelection] = useState("")
     const [dates, setDates] = useState(null)
+    const [fuel, setFuel] = useState("elec")
+    const [unit, setUnit] = useState("energy")
+    const today = new Date()
+    const month = today.getMonth()
+    const year = today.getFullYear()
+    const pastYear = year - 1
+
 
     useEffect(() => {
-        const getResponse = async () => {
+        console.log(today, month, year, pastYear)
+        const getBreakdown = async () => {
             return await fetch(
-                `/api/engagement/v2/breakdown/${householdID}/month?fuel=elec&units=energy`,
+                `/api/engagement/v2/breakdown/${householdID}/month?fuel=${fuel}&units=${unit}`,
                 {
                     method: "GET",
                     headers: { "Authorization": `Bearer ${access_token}` }
@@ -26,16 +34,17 @@ const Devices = () => {
                 .then(date => {
                     setDates(date[0])
                     fetch(
-                        `/api/engagement/v2/breakdown/${householdID}/month/${date[0]}?fuel=elec&units=energy`,
+                        `/api/engagement/v2/breakdown/${householdID}/month/${date[0]}?fuel=${fuel}&units=${unit}`,
                         {
                             method: "GET",
                             headers: { "Authorization": `Bearer ${access_token}` }
-                        }).then(res => res.json())
+                        })
+                        .then(res => res.json())
                         .then(data => {
                             let values = []
                             values.push(["Category", "Usage"])
                             Object.keys(data.values).forEach(item => {
-                                if(data.values[item][1] !== 0){
+                                if (data.values[item][1] !== 0) {
                                     values.push([item, Math.round(data.values[item][1] / 1000 * 100) / 100])
                                 }
                             });
@@ -47,24 +56,71 @@ const Devices = () => {
 
         }
 
-        getResponse()
-    }, [householdID, access_token])
+        getBreakdown()
+    }, [householdID, access_token, fuel, unit])
+
+    useEffect(() => {
+        const getMonths = async () => {
+            return await fetch(
+                `api/engagement/v2/consumption/${householdID}/monthly?fuel=${fuel}&units=${unit}`,
+                {
+                    method: "GET",
+                    headers: { "Authorization": `Bearer ${access_token}` }
+                })
+                .then(res => res.json())
+                .catch(err => console.error(err))
+        }
+
+        const getLastYearsUsage = async () => {
+            return await fetch(
+                `api/engagement/v2/consumption/${householdID}/monthly?fuel=${fuel}&units=${unit}`,
+                {
+                    method: "GET",
+                    headers: { "Authorization": `Bearer ${access_token}` }
+                })
+        }
+
+        const temp = [1, 2, 3, 4, 5, 4, 32]
+        const calcMean = (arr) => {
+            let total = 0;
+            arr.forEach(num => {
+                total += num;
+            })
+            return total / arr.length
+        }
+
+        console.log(calcMean(temp))
+
+        const getComparison = async () => {
+            return await fetch(
+                `api/engagement/v2/profile/${householdID}/peer-groups`,
+                {
+                    method: "GET",
+                    headers: { "Authorization": `Bearer ${access_token}` }
+                })
+                .then(res => console.log(res.json()))
+        }
+
+        getMonths()
+        getComparison()
+
+    }, [householdID, access_token, fuel, unit])
 
     const MainGraph = () => {
-        if(response === {}){
+        if (response === {}) {
             return <Loading />
         } else {
             return (
                 <Chart
                     width={'125%'}
                     height={'27.125em'}
-                    style={{display: 'flex', justifyContent: 'center', alignItems: 'flex-start', backgroundColor: 'transparent'}}
+                    style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', backgroundColor: 'transparent' }}
                     chartType="PieChart"
                     loader={<Loading />}
                     data={values}
                     options={{
                         legend: 'none',
-                        pieHole: 0.5,
+                        pieHole: 0.7,
                         pieSliceText: 'none',
                         backgroundColor: 'transparent',
                         colors: [primary, primaryDark, secondaryD, secondaryI, secondaryB, secondaryA, secondaryT]
@@ -87,14 +143,14 @@ const Devices = () => {
     }
 
     const Pagination = () => {
-        if(dates === null){
+        if (dates === null) {
             return <Loading />
         } else {
             return (<span className={styles.pagination}>
-                        <Button text="<" handleClick={() => handleClick()} />
-                            {dates}
-                        <Button text=">" handleClick={() => handleClick()} />
-                    </span>)
+                <Button text="<" handleClick={() => handleClick()} />
+                {dates}
+                <Button text=">" handleClick={() => handleClick()} />
+            </span>)
         }
     }
 
