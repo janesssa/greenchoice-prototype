49f3c0bf61601d51f0540e5e53259567a19c54c1
Janessa <0935165@hr.nl>
2021-05-25 14:49:45 +0200

    Added own comparison

--- next-app/pages/devices/index.tsx
+++ next-app/pages/devices/index.tsx
@@ -10,19 +10,21 @@ import Button from 'utilities/components/atoms/Button'
 
 const Devices = () => {
     const { householdID, access_token, response } = useHouseholdContext()
-    const [values, setValues] = useState([])
+    const [breakdown, setBreakdown] = useState([])
+    const [compareOwn, setCompareOwn] = useState([])
+    const [compareHouseholds, setCompareHouseholds] = useState([])
     const [selection, setSelection] = useState("")
     const [dates, setDates] = useState(null)
     const [fuel, setFuel] = useState("elec")
     const [unit, setUnit] = useState("energy")
     const today = new Date()
-    const month = today.getMonth()
+    const monthNumber = today.getMonth()
     const year = today.getFullYear()
     const pastYear = year - 1
 
 
     useEffect(() => {
-        console.log(today, month, year, pastYear)
+        console.log(today, monthNumber, year, pastYear)
         const getBreakdown = async () => {
             return await fetch(
                 `/api/engagement/v2/breakdown/${householdID}/month?fuel=${fuel}&units=${unit}`,
@@ -48,7 +50,7 @@ const Devices = () => {
                                     values.push([item, Math.round(data.values[item][1] / 1000 * 100) / 100])
                                 }
                             });
-                            setValues(values)
+                            setBreakdown(values)
                         })
                         .catch(err => console.error(err))
                 })
@@ -60,27 +62,6 @@ const Devices = () => {
     }, [householdID, access_token, fuel, unit])
 
     useEffect(() => {
-        const getMonths = async () => {
-            return await fetch(
-                `api/engagement/v2/consumption/${householdID}/monthly?fuel=${fuel}&units=${unit}`,
-                {
-                    method: "GET",
-                    headers: { "Authorization": `Bearer ${access_token}` }
-                })
-                .then(res => res.json())
-                .catch(err => console.error(err))
-        }
-
-        const getLastYearsUsage = async () => {
-            return await fetch(
-                `api/engagement/v2/consumption/${householdID}/monthly?fuel=${fuel}&units=${unit}`,
-                {
-                    method: "GET",
-                    headers: { "Authorization": `Bearer ${access_token}` }
-                })
-        }
-
-        const temp = [1, 2, 3, 4, 5, 4, 32]
         const calcMean = (arr) => {
             let total = 0;
             arr.forEach(num => {
@@ -89,8 +70,94 @@ const Devices = () => {
             return total / arr.length
         }
 
-        console.log(calcMean(temp))
+        const getConsumption = async () => {
+            return await fetch(
+                `api/engagement/v2/consumption/${householdID}/monthly/${year}?fuel=${fuel}&units=${unit}`,
+                {
+                    method: "GET",
+                    headers: { "Authorization": `Bearer ${access_token}` }
+                })
+                .then(res => res.json())
+                .then(data => {
+                    let month: string | number = monthNumber
+                    if (month < 10) {
+                        month = `0${monthNumber}`
+                    }
+                    return ["Deze maand", Math.round(data.consumption[`${year}-${month}`] / 1000 * 100) / 100, `color: ${secondaryI}`]
+                })
+                .catch(err => console.error(err))
+        }
 
+        const getMean = async () => {
+            const months = await fetch(
+                `api/engagement/v2/consumption/${householdID}/monthly?fuel=${fuel}&units=${unit}`,
+                {
+                    method: "GET",
+                    headers: { "Authorization": `Bearer ${access_token}` }
+                })
+                .then(res => res.json())
+                .catch(err => console.error(err))
+
+            const res = months.map(async year => {
+                return await fetch(
+                    `api/engagement/v2/consumption/${householdID}/monthly/${year}?fuel=${fuel}&units=${unit}`,
+                    {
+                        method: "GET",
+                        headers: { "Authorization": `Bearer ${access_token}` }
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        let days = Object.values(data.actualNumberOfDays).reduce((a: number, b: number) => a + b)
+                        let consumption = Object.values(data.consumption).reduce((a: number, b: number) => a + b)
+                        let dayAvg: number = (consumption as number) / (days as number)
+                        let monthAvg = Math.round((dayAvg * 30.5) / 1000 * 100) / 100
+                        return monthAvg
+                    })
+                    .catch(err => console.error(err))
+            })
+
+            const mean = await Promise.all(res).then(data => calcMean(data)).catch(err => console.error(err))
+            return ["Gemiddelde", mean, `color: ${secondaryA}`]
+        }
+
+        const getPastYearsUsage = async () => {
+            return await fetch(
+                `api/engagement/v2/consumption/${householdID}/monthly/${pastYear}?fuel=${fuel}&units=${unit}`,
+                {
+                    method: "GET",
+                    headers: { "Authorization": `Bearer ${access_token}` }
+                })
+                .then(res => res.json())
+                .then(data => {
+                    let month: string | number = monthNumber
+                    if (month < 10) {
+                        month = `0${monthNumber}`
+                    }
+                    let days = data.actualNumberOfDays[`${pastYear}-${month}`]
+                    let consumption = data.consumption[`${pastYear}-${month}`]
+                    let avg = consumption / days
+                    return [`Vorig jaar ${new Intl.DateTimeFormat('nl-NL', { month: "long" }).format(monthNumber)}`, Math.round(consumption / 1000 * 100) / 100, `color: ${primary}`]
+                })
+                .catch(err => console.error(err))
+        }
+
+        const getValues = async () => {
+            return await Promise.all([getMean(), getPastYearsUsage(), getConsumption()])
+                .then(data => {
+                    let values = []
+                    values.push(["Time", "Unit", {role: 'style'}])
+                    data.forEach(item => {
+                        values.push(item)
+                    })
+                    setCompareOwn(values)
+                })
+                .catch(err => console.error(err))
+        }
+
+        getValues().catch(console.error)
+    }, [householdID, access_token, fuel, unit])
+
+    useEffect(() => {
         const getComparison = async () => {
             return await fetch(
                 `api/engagement/v2/profile/${householdID}/peer-groups`,
@@ -98,16 +165,15 @@ const Devices = () => {
                     method: "GET",
                     headers: { "Authorization": `Bearer ${access_token}` }
                 })
-                .then(res => console.log(res.json()))
+                .then(res => res.json())
         }
 
-        getMonths()
         getComparison()
 
     }, [householdID, access_token, fuel, unit])
 
     const MainGraph = () => {
-        if (response === {}) {
+        if (breakdown.length === 0) {
             return <Loading />
         } else {
             return (
@@ -117,7 +183,7 @@ const Devices = () => {
                     style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', backgroundColor: 'transparent' }}
                     chartType="PieChart"
                     loader={<Loading />}
-                    data={values}
+                    data={breakdown}
                     options={{
                         legend: 'none',
                         pieHole: 0.7,
@@ -130,11 +196,32 @@ const Devices = () => {
                         callback: ({ chartWrapper }) => {
                             const chart = chartWrapper.getChart()
                             const selection = chart.getSelection()
-                            setSelection(values[selection[0].row + 1])
+                            setSelection(breakdown[selection[0].row + 1])
                         }
                     }]}
                 />
             )
+        }
+    }
+
+    const CompareOwnGraph = () => {
+        if (compareOwn.length === 0) {
+            return <Loading />
+        } else {
+            return <Chart
+                width={'18em'}
+                height={'17.5em'}
+                style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', backgroundColor: 'transparent' }}    
+                chartType="Bar"
+                loader={<Loading />}
+                data={compareOwn}
+                options={{
+                    titlePosition: 'none',
+                    hAxis: { textPosition: 'none' },
+                    legend: { position: 'none'} ,
+                    bar: { groupWidth: "25%" }
+                }}
+            />
         }
     }
 
@@ -160,7 +247,9 @@ const Devices = () => {
                 <Pagination />
                 <MainGraph />
             </Card>
-            <Card type='title' title='Vergelijk jezelf'></Card>
+            <Card type='title' title='Vergelijk jezelf' btntext="Bekijk de vergelijking">
+                <CompareOwnGraph />
+            </Card>
             <Card type='title' title='Vergelijkbare huishoudens'></Card>
         </Layout>
     )
