bd6e4bd4a57331b7f820c2995b1d7c25fe75944c
Janessa <0935165@hr.nl>
2021-05-26 16:43:07 +0200

    fixed catch of proxy

--- next-app/pages/api/[...all].ts
+++ next-app/pages/api/[...all].ts
@@ -4,11 +4,16 @@ import httpProxyMiddleware from 'next-http-proxy-middleware';
 const API_SERVICE_URL = "https://api.onzo.io/";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('api', req.url)
-  return httpProxyMiddleware(req, res, {
-    target: API_SERVICE_URL,
-    pathRewrite: {
-        '^/api':'/'
+  return new Promise<void>(resolve => {
+    if(req.method === "GET"){
+      return httpProxyMiddleware(req, res, {
+        target: API_SERVICE_URL,
+        pathRewrite: {
+          '^/api': '/'
+        }
+      }).catch(console.error)
     }
+    res.status(405).end()
+    return resolve()
   })
 }
