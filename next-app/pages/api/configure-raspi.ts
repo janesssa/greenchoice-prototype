0fde4fd04df6d5876a674a42db544e81c756fc53
Janessa <0935165@hr.nl>
2021-07-05 13:26:49 +0200

    Added File adding

--- next-app/pages/api/configure-raspi.ts
+++ next-app/pages/api/configure-raspi.ts
@@ -1,10 +1,12 @@
 import { exec } from 'child_process';
+import fs from 'fs'
 
 const findRaspi = () => {
     return new Promise((resolve, reject) => {
         exec('ping -c 1 greenchoicepi', (error, stdout, stderr) => {
             if (error) {
                 console.error(error);
+                resolve('192.168.2.60');
             }
             if (stdout) {
                 const r = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
@@ -12,7 +14,6 @@ const findRaspi = () => {
                 resolve(ip)
             }
 
-            resolve('0.0.0.0');
         });
     });
 }
@@ -31,7 +32,7 @@ const createVPNProfile = () => {
     //     console.log('RES '+ JSON.stringify(res))
     // }).catch(console.error)
 
-    return 'https://168.119.59.191/key/QOTrtHEw5SoU1pmL8sIFFPrkLTvNu7hp.tar'
+    return 'https://168.119.59.191/key/IUb177yD7PUWVUzL0Gt5tNirZ5pDAxUg.tar'
 }
 
 export default async (req, res) => {
@@ -39,24 +40,63 @@ export default async (req, res) => {
 
     const internalIP = await findRaspi()
     console.log(`Internal IP: ${internalIP}`)
-    
+
     const link = createVPNProfile()
     console.log(`Download link: ${link}`)
-    
-    await fetch(`http://${internalIP}:9876/create-profile`, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({ 'url': link })
-        }).catch(console.error)
-    
-    const externalIP = await fetch(`http://${internalIP}:9876/current-ip`)
-        .then(res => res.json())
-        .catch(console.error)
-    console.log('External IP: ' + externalIP.response[0])
 
-    res.send(JSON.stringify({
-        "status": 200,
-        "error": null,
-        "response": externalIP.response[0]
-    }))
+    await fetch(`http://${internalIP}:9876/create-profile`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 'url': link })
+    }).catch(console.error)
+
+    const externalIP = () => {
+        let data
+        try {
+            data = fs.readFileSync('/usr/app/utilities/ip.json')
+        } catch (err) {
+            console.log(err)
+            console.log('Continuing by fetching the IP')
+            return fetch(`http://${internalIP}:9876/current-ip`)
+                .then(res => res.json())
+                .then(data => {
+                    fs.writeFile("/usr/app/utilities/ip.json", JSON.stringify({ 'ip': data.response[0] }), function (err) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        console.log("The file was saved!");
+                    });
+                    return data.response[0]
+                })
+                .catch(console.error)
+
+        }
+
+        console.log('data: ' + data.toString())
+        const string = JSON.parse(data.toString())
+        console.log(typeof string)
+        // const ip = string.json()
+        console.log('ip: ' + string)
+        return string.ip
+    }
+
+    const exIP = await externalIP()
+    console.log('External IP: ' + exIP)
+    if (exIP === internalIP) {
+        externalIP()
+
+        if (exIP != undefined) {
+            res.send(JSON.stringify({
+                "status": 200,
+                "error": null,
+                "response": exIP
+            }))
+        } else {
+            res.send(JSON.stringify({
+                "status": 502,
+                "error": "Something went wrong!"
+            }))
+        }
+
+    }
 }
