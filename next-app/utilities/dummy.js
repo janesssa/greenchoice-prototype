d0e40e5afad565af8ad61860a79c5cc759581c82
Janessa <0935165@hr.nl>
2021-07-06 11:18:32 +0200

    dummy data

---
+++ next-app/utilities/dummy.js
@@ -0,0 +1,59 @@
+const data = {
+    "date": "April 2021",
+    "Totale verbruik": {
+        "Deze maand": "72.87 kWh",
+        "amount": "€ 15,51",
+        "percentage": "100%",
+        "Vorig jaar": 75.18,
+        "Gemiddelde": 73.52,
+        "Jouw verbruik": 72.87,
+        "Soortgelijke huishoudens": 80.3
+    },
+    "Verlichting": {
+        "Deze maand": "10.93 kWh",
+        "amount": "€ 2,45",
+        "percentage": "15%",
+        "Vorig jaar": 8.26,
+        "Gemiddelde": 9.2,
+        "Jouw verbruik": 10.93,
+        "Soortgelijke huishoudens": 8.32
+    },
+    "Wassen": {
+        "Deze maand": "14.57 kWh",
+        "amount": "€ 3,10",
+        "percentage": "20%",
+        "Vorig jaar": 15.2,
+        "Gemiddelde": 14.83,
+        "Jouw verbruik": 14.57,
+        "Soortgelijke huishoudens": 15.8
+    },
+    "Koken": {
+        "Deze maand": "18.21 kWh",
+        "amount": "€ 3,80",
+        "percentage": "25%",
+        "Vorig jaar": 17.6,
+        "Gemiddelde": 18,
+        "Jouw verbruik": 18.21,
+        "Soortgelijke huishoudens": 18.12
+    },
+    "Overig": {
+        "Deze maand": "25.5 kWh",
+        "amount": "€ 5,42",
+        "percentage": "35%",
+        "Vorig jaar": 23.51,
+        "Gemiddelde": 24.03,
+        "Jouw verbruik": 25.5,
+        "Soortgelijke huishoudens": 28.6
+    },
+    "Sluiverbruik": {
+        "Deze maand": "3.65 kWh",
+        "amount": "€ 0,95",
+        "percentage": "5%",
+        "Vorig jaar": 5.18,
+        "Gemiddelde": 4.6,
+        "Jouw verbruik": 3.65,
+        "Soortgelijke huishoudens": 3.8
+    }
+}
+
+export default data
